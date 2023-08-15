612502647fe0435efd3ccf6b92172d4a169cfc1a
AndrianovDM <85298557+AndrianovDM@users.noreply.github.com>
2023-08-16 01:02:23 +0300

    Add files via upload

--- streamlitFunction.py
+++ streamlitFunction.py
@@ -39,6 +39,7 @@ def check_password():
         return False
     else:
         # Password correct.
+        st.session_state.number_of_steps_ = 0.0
         return True
 
 def Save_to_file_stage(stage, name , extension ):
